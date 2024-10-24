be18bc48c3a0941a6f644fe21fd314d1061169a3
yasfaa <yasfanaruto@gmail.com>
2024-10-24 17:06:34 +0700

    feat add invoices datatable with query string & pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,6 @@ import LatestInvoices from "../../ui/dashboard/latest-invoices"
 import { lusitana } from "../../ui/fonts"
 import { Suspense } from "react"
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons"
-import { fetchCardData } from "../../lib/data"
 
 export default async function Page() {
     return (
@@ -13,7 +12,7 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            <Suspense fallback={<CardsSkeleton />}>
+                <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
             </div>
